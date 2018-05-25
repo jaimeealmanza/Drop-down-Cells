6591d535fc9dbf3d41aae3f0abfbfcf563887159
Jaime E. Almanza <almanza.jaime@icloud.com>
2018-05-24 21:26:47 -0600

    General clean-up complete

--- Drop-down Cells/Drop-down Cells/View Controllers/TableViewController.swift
+++ Drop-down Cells/Drop-down Cells/View Controllers/TableViewController.swift
@@ -16,34 +16,34 @@ class TableViewController: UITableViewController {
     
     // MARK: - Properties
     
-    struct CellData {
+    struct CustomSection {
         var opened = Bool()
         var title = String()
-        var sectionData = [String]()
+        var cells = [String]()
     }
     
-    var data = [CellData]()
+    var mainSections = [CustomSection]()
 
     
     // MARK: - Lifecycle Functions & Initial setup
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        data = [CellData(opened: false, title: "Section 1", sectionData: ["Drop-down cell 1", "Drop-down cell 2", "Drop-down cell 3"]),
-                CellData(opened: false, title: "Section 2", sectionData: ["Drop-down cell 1", "Drop-down cell 2", "Drop-down cell 3"]),
-                CellData(opened: false, title: "Section 3", sectionData: ["Drop-down cell 1", "Drop-down cell 2", "Drop-down cell 3"])]
+        mainSections = [CustomSection(opened: false, title: "Section 1", cells: ["Sec. 1 Drop-down cell 1", "Sec. 1 Drop-down cell 2", "Sec. 1 Drop-down cell 3"]),
+                        CustomSection(opened: false, title: "Section 2", cells: ["Sec. 2 Drop-down cell 1", "Sec. 2 Drop-down cell 2", "Sec. 2 Drop-down cell 3"]),
+                        CustomSection(opened: false, title: "Section 3", cells: ["Sec. 3 Drop-down cell 1", "Sec. 3 Drop-down cell 2", "Sec. 3 Drop-down cell 3"])]
     }
     
     
     // MARK: - TableView
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return data.count
+        return mainSections.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if data[section].opened == true {
-            return data[section].sectionData.count + 1 // Plus one to account for the first drop-down cell
+        if mainSections[section].opened == true {
+            return mainSections[section].cells.count + 1 // Plus one to account for the first drop-down cell
         } else {
             return 1 // This is the section cell containing the others
         }
@@ -52,23 +52,23 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
-            cell.textLabel?.text = data[indexPath.section].title
+            cell.textLabel?.text = mainSections[indexPath.section].title
             return cell
         } else {
             // Use a custom class cell with its unique identifier if needed
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
-            cell.textLabel?.text = data[indexPath.section].sectionData[indexPath.row - 1] // Minus one since we have cells in total (including the section cell) but only three elements as sectionData
+            cell.textLabel?.text = mainSections[indexPath.section].cells[indexPath.row - 1] // Minus one since we have cells in total (including the section cell) but only three elements as sectionData
             return cell
         }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if data[indexPath.section].opened == true {
-            data[indexPath.section].opened = false
+        if mainSections[indexPath.section].opened == true {
+            mainSections[indexPath.section].opened = false
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none) // Use whichever animation you like
         } else {
-            data[indexPath.section].opened = true
+            mainSections[indexPath.section].opened = true
             let sections = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(sections, with: .none) // Use whichever animation you like
         }
